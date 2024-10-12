d388d23fb8496768b7fe34eaa9b509cd43002d80
adamhwang <adamhwang@users.noreply.github.com>
2024-10-12 23:54:41 +0000

    deploy: f387fd429b984fd0722f148b84ee9293be26694e

--- _next/static/5hx5gCCb5SWHFXndFHJJ-/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-b372ba5c9516a748.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
